aba7f930f52aa271a7c6e1be812aa4e16602c5f1
Bhavek Budhia <bhavek.budhia@invenia.ca>
2012-09-12 20:11:51 -0500

    emel.py can now call functions from the emel directory. init.py now sets the location of the data directory

--- emel.py
+++ emel.py
@@ -2,21 +2,45 @@ import sys
 import imp
 from glob import glob
 import re
+import os
 
 def list_all_actions():
-    info = imp.find_module('emel')
-    module = imp.load_module('emel', *info)
+    allActions = {}
+
+    emelInfo = imp.find_module('emel')
+    emelModule = imp.load_module('emel', *emelInfo)
 
     files = glob('emel/*.py')
 
+    moduleNames = [ os.path.splitext(os.path.split(f)[-1])[0] for f in files if not f.startswith('__') and not f.endswith('__.py') ]
 
+    for moduleName in moduleNames:
+        try:
+            moduleInfo = imp.find_module(moduleName, emelModule.__path__)
+            currModule = imp.load_module(moduleName, *moduleInfo)
+            allActions[moduleName] = currModule
+
+        except Exception as e:
+            print 'Emel failed with errors:'
+            print '{0} {1}'.format(type(e), str(e))
+            exit(1)
     
-
+    return allActions
 
 def main(argv):
     '''
     '''
+    if argv:
+        actions = list_all_actions()
+        if argv[0] not in actions:
+            print '\tUnknown command "{0}"'.format(argv[0])
+            print '\t please try'
+            for action in actions:
+                print '\t\t',action
+            exit(1)
 
+        else:
+            actions[argv[0]].main(argv[1:])
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- emel/init.py
+++ emel/init.py
@@ -1,23 +1,48 @@
 import sys
 import argparse
+import os
+from utils.settings.configobj import ConfigObj
+
+def Initialize_emel(options):
+    '''
+    Sets the location of the data directory in the emel.config file.
+    '''
+
+    # 1. create the data directory
+    dataDir = options.location + os.sep + 'Data'
+    if not os.path.isdir(dataDir):
+        print 'Creatint Data folder at', dataDir
+        os.makedirs(dataDir)
+    else:
+        print 'Directory', dataDir, 'allready exists'
+
+    config = ConfigObj()
+    config.filename = os.getcwd() + os.sep + 'emel.conf'
+    config['core'] = {}
+    config['core']['dataDir'] = dataDir
+
+    config.write()
+
+    
 
 def setup_arg_parser():
     '''
     Create the argument parser
     '''
-    parser = argparse.ArgumentParser(description='This module initializes emels core.')
+    parser = argparse.ArgumentParser(description='This module sets the location of the emel Data directory.')
 
+    parser.add_argument('-l', '--location', action='store',  
+                    dest='location', default=os.getcwd(), help='Sets the location of the data directory. Defaults to the current working directory.')
     return parser
-
-
 
 
 def main(argv):
     '''
     '''
-
     parser = setup_arg_parser()
-    parser.parse_args(argv)
+    options = parser.parse_args(argv)
+    Initialize_emel(options)
+
 
 if __name__=='__main__':
     main(sys.argv[1:])
