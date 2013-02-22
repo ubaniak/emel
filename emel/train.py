c6d76468024eecf09cbc99a434ecf095da27295c
Ubaniak <bhavek.budhia@gmail.com>
2013-02-21 23:33:51 -0600

    added path to project tools

--- emel/train.py
+++ emel/train.py
@@ -16,6 +16,9 @@ def __validate_config__():
 
 TRAIN_TEMPLATE = """
 import sys
+# Add a path to project specific tools.
+sys.path.append('{}')
+import tools as project_tools
 
 class Train(object):
     def gather_data(self):
@@ -30,7 +33,7 @@ def __create_train__():
     config = __validate_config__()
 
     trainPath = emel_train_file_path()
-    print emel_project_tools_file_path()
+    train_object = TRAIN_TEMPLATE.format(emel_project_tools_file_path())
 
     print 'Creating train object ...',
     with open( create_path([trainPath, 'train.py']), 'w' ) as fp:
