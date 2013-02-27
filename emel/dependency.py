2aad5ddaa30c1d0671fddbbf6280d0418f1c2269
Ubaniak <bhavek.budhia@gmail.com>
2013-02-27 14:17:08 -0600

    Created the dependency installer

--- emel/dependency.py
+++ emel/dependency.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import os
 import re
+import subprocess
 import imp
 
 from utils.settings.configobj import ConfigObj
@@ -24,20 +25,70 @@ def __validate_config__():
     if Project.CURRENT not in config[Project.SECTION]: config[Project.SECTION][Project.CURRENT] = ''
     return config
 
+# Check if pip or easy_install is Installed
+def __get_installer__():
+    easy_install = subprocess.call(['easy_install','--version'])
+    pip = subprocess.call(['pip','--version'])
 
-def __list_dependencies__(verbose=False):
+    if easy_install != 0 and pip != 0:
+        print 'ERROR: cannot find easy_install or pip.'
+        exit()
+    elif easy_install == 0 and pip == 0:
+        # pip is the installer of choice.
+        installer = 'pip'
+    elif easy_install != 0 and pip == 0:
+        installer = 'easy_install'
+    elif easy_install == 0 and pip != 0:
+        installer = 'pip'
+    return installer
+
+
+def __get_list__():
     the_list = DEFULT
     if check_project_status():
-        the_list = []
         depFile = create_path([emel_project_path(), DEPENDENCY_FILE])
+        if os.path.exists(depFile):
+            the_list = []
+            with open(depFile, 'r') as f:
+                for line in f.readlines():
+                    if not line.startswith('#'):
+                        line = line.split('#')
+                        the_list.append(line[0].strip())
+    return the_list
 
-        with open(depFile, 'r') as f:
-            for line in f.readlines():
-                if not line.startswith('#'):
-                    line = line.split('#')
-                    the_list.append(line[0].strip())
+def __sanitize_moduel__(module):
+    regex = re.compile(';|&|-r')
+    m = regex.search(module)
+    if m:
+        return False
+    else:
+        return True
+
+
+def __install_dependencies__(verbose=False):
+    message = ('WARNING: This will install all listed dependencies.\n'
+               'Please make sure you are sudo.')
+    go = yes_no_option(message)
+    if go:
+        installer = __get_installer__()
+
+        for module in __get_list__():
+            print '\n*****Installing:', module,'*****\n'
+            if __sanitize_moduel__(module):
+                status = subprocess.call([installer, 'install', module])
+                if status != 0:
+                    print '\n*****Failed to install', module,'*****'
+                else:
+                    print '\n*****Successflly installed', module,'*****'
+            else:
+                print 'Rejecting', module
+    else:
+        print 'Aborting.'
+
+def __list_dependencies__(verbose=False):
+
     can_run = True
-    for module in the_list:
+    for module in __get_list__():
         try:
             imp.find_module(module)
             if verbose:
@@ -80,6 +131,8 @@ def setup_arg_parser():
                     dest='new', help='Creates a new dependency file in the current project directory.')
     parser.add_argument('-ls', '--list', action='store_true', 
                     dest='listDeps', help='Checks the list of dependencies.')
+    parser.add_argument('--install', action='store_true', 
+                    dest='install', help='Install the selected modules.')
     return parser
 
 
@@ -93,6 +146,8 @@ def main(argv):
         __list_dependencies__(True)
     elif options.new:
         __create_dep_list__()
+    elif options.install:
+        __install_dependencies__()
 
 
 if __name__=='__main__':
